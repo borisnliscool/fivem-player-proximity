fba416c97d00d656c06e46de33abb9c592e38159
borisnliscool <60477582+borisnliscool@users.noreply.github.com>
2023-08-09 22:47:17 +0200

    working

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,15 @@
+use eframe::egui;
 use hotwatch::{
     blocking::{Flow, Hotwatch},
     Event,
 };
 use lazy_static::lazy_static;
 use regex::Regex;
-use std::collections::HashMap;
-use std::env;
+use std::{env, io::{Read, Seek, SeekFrom}};
 use std::fs;
 use std::path::PathBuf;
 use std::sync::Mutex;
+use std::{collections::HashMap, thread};
 
 fn get_logs_folder() -> Result<String, env::VarError> {
     let local_app_data = env::var("LOCALAPPDATA")?;
@@ -44,15 +45,6 @@ lazy_static! {
     static ref NEARBY_PLAYERS: Mutex<HashMap<String, Player>> = Mutex::new(HashMap::new());
 }
 
-fn print_all_players() {
-    let players = NEARBY_PLAYERS.lock().unwrap();
-
-    println!("All Players:");
-    for (_, plr) in players.iter() {
-        println!("{}", plr.name);
-    }
-}
-
 fn process_log_diff(last_contents: &str, new_contents: &str) {
     let diff = difference::Changeset::new(last_contents, new_contents, "\n");
     let enter_regex = Regex::new(r"Creating physical player \d+ \((?P<name>.+?)\)").unwrap();
@@ -78,28 +70,67 @@ fn process_log_diff(last_contents: &str, new_contents: &str) {
 
                     let mut players = NEARBY_PLAYERS.lock().unwrap();
                     players.remove(name);
-                }
-
-                print_all_players();
+                };
             }
             _ => {}
         }
     }
 }
 
-fn main() -> Result<(), failure::Error> {
-    let logs_folder = get_logs_folder()?;
-    let latest_log_file = get_latest_log_path(&logs_folder).expect("Couldn't find log file");
+struct MyApp {}
 
-    let mut last_log_contents = String::new();
+impl Default for MyApp {
+    fn default() -> Self {
+        Self {}
+    }
+}
 
+impl eframe::App for MyApp {
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        let players = NEARBY_PLAYERS.lock().unwrap();
+        egui::CentralPanel::default().show(ctx, |ui| {
+            ui.heading(format!("Nearby Players ({})", players.len()));
+            // Iterate over player names and print them
+            ui.vertical(|ui| {
+                for player in players.values() {
+                    ui.label(player.name.clone());
+                }
+            });
+
+            ctx.request_repaint();
+        });
+    }
+}
+
+fn run_ui() {
+    let options = eframe::NativeOptions {
+        initial_window_size: Some(egui::vec2(320.0, 240.0)),
+        ..Default::default()
+    };
+    let _ = eframe::run_native(
+        "FiveM nearby player detector",
+        options,
+        Box::new(|_cc| Box::<MyApp>::default()),
+    );
+}
+
+fn watch_log_file(log_file: &PathBuf, mut last_file_size: u64) -> Result<(), failure::Error> {
     let mut watcher = Hotwatch::new()?;
-    watcher.watch(&latest_log_file, move |event: Event| {
-        if let Ok(contents) = fs::read_to_string(&event.paths[0]) {
-            if !last_log_contents.is_empty() {
-                process_log_diff(&last_log_contents, &contents);
+    watcher.watch(log_file, move |event: Event| {
+        if let Ok(metadata) = fs::metadata(&event.paths[0]) {
+            let new_file_size = metadata.len();
+            if new_file_size > last_file_size {
+                let file = fs::File::open(&event.paths[0]);
+                if let Ok(mut file) = file {
+                    if file.seek(SeekFrom::Start(last_file_size)).is_ok() {
+                        let mut new_contents = String::new();
+                        if file.read_to_string(&mut new_contents).is_ok() {
+                            process_log_diff("", &new_contents); // Compare with empty string for initial case
+                            last_file_size = new_file_size;
+                        }
+                    }
+                }
             }
-            last_log_contents = contents;
         }
 
         Flow::Continue
@@ -109,4 +140,30 @@ fn main() -> Result<(), failure::Error> {
     watcher.run();
 
     Ok(())
+}
+
+
+fn run_watcher() -> Result<(), failure::Error> {
+    let logs_folder = get_logs_folder()?;
+    let latest_log_file = get_latest_log_path(&logs_folder).expect("Couldn't find log file");
+
+    let initial_file_size = fs::metadata(&latest_log_file)?.len();
+    watch_log_file(&latest_log_file, initial_file_size)?;
+
+    Ok(())
+}
+
+fn main() {
+    // Spawn the file watcher thread
+    let file_watcher = thread::spawn(move || {
+        if let Err(err) = run_watcher() {
+            eprintln!("File watcher error: {}", err);
+        }
+    });
+
+    // Run the UI on the main thread
+    run_ui();
+
+    // Wait for the file watcher thread to finish
+    let _ = file_watcher.join().expect("File watcher thread panicked");
 }
